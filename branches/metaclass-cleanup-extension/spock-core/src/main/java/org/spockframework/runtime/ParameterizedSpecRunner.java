178267173005839b9d95bcc07c7d559ef4d7c40b
ldaley <ldaley@a23d8be2-3f2b-0410-95c2-272da069bfd2>
2010-08-15 07:44:44 +0000

    Issue 120 - Set the parent of IterationInfo to the FeatureInfo it belongs to.

--- branches/metaclass-cleanup-extension/spock-core/src/main/java/org/spockframework/runtime/ParameterizedSpecRunner.java
+++ branches/metaclass-cleanup-extension/spock-core/src/main/java/org/spockframework/runtime/ParameterizedSpecRunner.java
@@ -115,6 +115,7 @@ public class ParameterizedSpecRunner extends BaseSpecRunner {
 
     while (haveNext(iterators)) {
       IterationInfo iteration = new IterationInfo(nextArgs(iterators), estimatedNumIterations);
+      iteration.setParent(currentFeature);
       runIteration(iteration);
 
       if (resetStatus(ITERATION) != OK) break;
